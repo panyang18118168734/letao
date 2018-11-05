85a5ce486ce067e25e9760f275e18cff6550d4b0
panyang18118168734 <944700720@qq.com>
2018-11-05 22:29:08 +0800

    购物车页面完成

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -1,18 +1,38 @@
-$(function () {  
-  $.ajax({
-    type: "get",
-    url: "/cart/queryCart",
-    dataType: "json",
-    success: function (info) {
-      
-      var str =template('tmp',{list:info})
-      console.log({list:info});
-      $('.lt_main .mui-scroll').html(str);
-    }
-  });
+$(function () {
+  render()
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/cart/queryCart",
+      dataType: "json",
+      success: function (info) {
+        
+        var str =template('tmp',{list:info})
+        console.log({list:info});
+        $('.lt_main .mui-scroll').html(str);
+      }
+    });
+    
+  }
 
 
-  $('#')
+  $('.lt_main .mui-scroll').on('click','.btn_delete',function () {  
+    var id= $(this).data('id');
+    // console.log(id);
+    $.ajax({
+      type: "get",
+      url: "/cart/deleteCart",
+      data: {
+        id:[id]
+      },
+      dataType: "json",
+      success: function (info) {
+        // console.log(info);
+        render()
+      }
+    });
+    
+  })
 
 
 
